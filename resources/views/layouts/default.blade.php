fea7732db0b0919439fa649d9729c73ec92c12cf
fahmirzl <fahmirizal.dev@gmail.com>
2025-04-05 06:49:26 +0700

    fix(layout): fix css directory bug

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ecommerce @yield('title')</title>
-    <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="shortcut icon" href="favicon.png">
     @stack('styles')
 </head>

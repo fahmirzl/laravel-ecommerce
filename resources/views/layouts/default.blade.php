8834c46ab8869ae018d65c5a0602bffaba7f58b7
fahmirzl <fahmirizal.dev@gmail.com>
2025-04-04 17:21:27 +0700

    feat: add favicon and replacement

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
+    <title>Ecommerce @yield('title')</title>
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="favicon.png">
     @stack('styles')
 </head>
 <body>

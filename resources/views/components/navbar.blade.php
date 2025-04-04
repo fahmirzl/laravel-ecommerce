a54f6aa24aae61c323dcbf4e67dd7cdcacee0e5e
fahmirzl <fahmirizal.dev@gmail.com>
2025-04-04 12:55:38 +0700

    initial commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+    <div class="container">
+        <div class="nav-items">
+            <div class="nav-logo">
+                <img src="./ecommerce-logo.svg" alt="E-Commerce Logo">
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.default')
+
+@section('title', 'Ecommerce')
+
+@section('content')
+    <x-navbar></x-navbar>
+@endsection
+
+@push('styles')
+    <style>
+
+    </style>
+@endpush
+
+@push('scripts')
+    <script>
+
+    </script>
+@endpush

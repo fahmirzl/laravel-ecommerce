c09b1acd239cc52231de785ae74a1605ab039122
fahmirzl <fahmirizal.dev@gmail.com>
2025-04-04 13:39:43 +0700

    feat: add navigation bar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,6 +4,20 @@
             <div class="nav-logo">
                 <img src="./ecommerce-logo.svg" alt="E-Commerce Logo">
             </div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Categories <span class="dropdown">›</span></a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div class="CTAs">
+                <div class="search-box">
+                    <img src="./search.svg" alt="search-logo" class="search-logo">
+                    <input type="text" class="search-input" placeholder="Search products">
+                </div>
+                <img src="./keranjang.svg" alt="" class="keranjang-icon">
+                <img src="./pengguna.svg" alt="" class="pengguna-icon">
+            </div>
         </div>
     </div>
 </nav>

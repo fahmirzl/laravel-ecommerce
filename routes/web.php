f3afeb8cff0aceb9c49a295fcd8d4bb0d5f86abb
fahmirzl <fahmirizal.dev@gmail.com>
2025-04-05 09:09:14 +0700

    feat: Create admin dashboard page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
+Route::prefix('v1')->group(function() {
+
+    Route::prefix('admin')->group(function() {
+        Route::get('/', AdminController::class);
+    });
+
+});
